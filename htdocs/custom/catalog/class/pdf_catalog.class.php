55e7df1103d926cc88028603939de5169e90e3aa
MichaelJeanmotte <michael.jeanmotte@gmelectronics.be>
2024-01-23 09:46:04 +0100

    Modify catalog: order by categories

--- htdocs/custom/catalog/class/pdf_catalog.class.php
+++ htdocs/custom/catalog/class/pdf_catalog.class.php
@@ -391,8 +391,8 @@ class pdf_catalog
 
 			if (is_readable($cover))
 			{
-				$height = 414;
-				$width = 300;
+				$height = 740;
+				$width = 500;
 				include_once DOL_DOCUMENT_ROOT . '/core/lib/images.lib.php';
 				$pdf->Image($cover, 0, 0, $width, $height);
 			}
